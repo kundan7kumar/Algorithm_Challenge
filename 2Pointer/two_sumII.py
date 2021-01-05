61d723074d33cca6ed55a79beadc9b87b1d34245
kundan kumar <kkundan7kumar@gmail.com>
2021-01-05 02:54:00 -0600

    Update code

--- 2Pointer/two_sumII.py
+++ 2Pointer/two_sumII.py
@@ -1,3 +1,17 @@
 # Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a
 # specific target number. The function twoSum should return indices of the two numbers such that they add up to the
 # target, where index1 must be less than index2
+
+a='4'
+d=float(a)
+b=2
+c=d+b
+print(c)
+# cmake a calculator :addition, subtarction,division,multiply
+float
+int
+double
+string
+
+# Python 3, anaconda
+git desktop
